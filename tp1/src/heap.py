e1df9d19ba5c9be0c441dfb22f8d115c9b60dff2
Alejandro Pernin <apernin@onapsis.com>
2023-05-01 23:19:01 -0300

    - Translated to English most of my code - Create a notebook to compare deque vs lists operations - Improve code for mege-sort using iterators

--- tp1/src/heap.py
+++ tp1/src/heap.py
@@ -1,68 +1,65 @@
-class Heap():
-    def __init__(self):
-        self.heap = []
-        self.size = 0
+"""
+Heap Sorting K-Sorted Arrays
 
-    def heapify(self, element_idx):
-        left_child = 2 * element_idx + 1
-        right_child = 2 * element_idx + 2
-        largest_idx = element_idx
+Time Complexity Considerations:
 
-        if((left_child < self.size) and (self.heap[left_child] < self.heap[largest_idx])):
-            largest_idx = left_child
-        
-        if((right_child < self.size) and (self.heap[right_child] < self.heap[largest_idx])):
-            largest_idx = right_child
-        
-        if(largest_idx != element_idx):
-            self.heap[largest_idx], self.heap[element_idx] = self.heap[element_idx], self.heap[largest_idx]
-            self.heapify(largest_idx)
+  We've taken into account Python Official docs https://wiki.python.org/moin/TimeComplexity for builtin classes like
+  list and deque; and heapq code https://github.com/python/cpython/blob/3.10/Lib/heapq.py for the heapq module.
 
-    def insert_element(self, new_element):
-        self.heap.append(new_element)
-        self.size = len(self.heap)
-        if(self.size != 1):
-            for idx in range((self.size // 2) - 1, -1, -1):
-                self.heapify(idx)
-
-    def remove_element(self, element_idx):
-        if(element_idx > self.size - 1):
-            return
-        self.heap[element_idx], self.heap[self.size-1] = self.heap[self.size-1], self.heap[element_idx]
-        removed_element = self.heap[self.size-1]
-        self.heap = self.heap[0:self.size-1]
-        
-        self.size -= 1
-        for idx in range((self.size // 2) -1, -1, -1):
-            self.heapify(idx)
-        return removed_element
+  - list:
+    - append: O(1)
+    - length: O(1)
+  - deque:
+    - popleft: O(1)
+    - length: O(1)
+  - heapq:
+    - heapify: O(log(N))
+    - heappop: O(log(N))
+    - heappush: O(log(N))
+"""
+import heapq
+from math import log2
+from typing import List, Deque, Tuple
 
 
-def k_merge_heap(arrays, heap, result):
-    if(len(arrays) == 0):
-        for i in range(len(heap.heap)):
-            result.append(heap.remove_element(0))
-        return
-    smallest = heap.remove_element(0)
-    for i in arrays:
-        if(i[0] == smallest):
-            i.pop(0)
-            if(len(i) > 0):
-                heap.insert_element(i[0])
-            break
-    arrays = [element for element in arrays if element != []]
-    result.append(smallest)
-    return k_merge_heap(arrays, heap, result)
+def heap_sort(arrays: List[Deque[int]]) -> Tuple[List[int], int]:
+    """
+     - Arrays: List of K ordered dequeues of H elements.
+    """
+    non_empty_list_count = len(arrays)  # O(1)
 
-
-def heap_sort(listas):
-    arrays = [list(sublist) for sublist in listas]
+    K = len(arrays)
     result = []
-    if(len(arrays) == 1):
-        return arrays
-    heap = Heap()
-    for i in arrays:
-        heap.insert_element(i[0])
-        continue
-    k_merge_heap(arrays, heap, result)
-    return result
+
+    """
+    Heap operations are log_2(n) and since we use a fixed size heap, all operations for heap
+    will be of cost log_2(K)
+    """
+    LOG_K = log2(K)
+
+    """
+    Construct the initial heap by removing the first element of each deque: O(K*1)
+    """
+    heap = [(n, arrays[i]) for i, n in enumerate(l.popleft() for l in arrays)]
+    heapq.heapify(heap)  # O(K)
+
+    ops = 0
+
+    while non_empty_list_count:  # O(K*H)
+        smallest, lst = heapq.heappop(heap)  # O(log(K))
+        result.append(smallest)  # O(1)
+
+        if lst:
+            # There are still elements in the original deque
+            heapq.heappush(heap, (lst.popleft(), lst))  # O(log(K))
+        else:
+            # The deque is empty
+            non_empty_list_count -= 1
+
+        """
+        In each loop at most we perform two O(log(K)) operations,
+        one heappop and one heappush
+        """
+        ops += LOG_K
+
+    return result, ops
--- tp1/src/merge_sorts.py
+++ tp1/src/merge_sorts.py
@@ -1,33 +1,107 @@
-def k_merge(left, right):
+"""
+Merge Sorting K-Sorted Arrays
+
+Time Complexity Considerations:
+
+  We've taken into account Python Official docs https://wiki.python.org/moin/TimeComplexity for builtin classes like
+  list and deque; and heapq code https://github.com/python/cpython/blob/3.10/Lib/heapq.py for the heapq module.
+
+  - list:
+    - append: O(1)
+    - length: O(1)
+  - deque:
+    - popleft: O(1)
+    - length: O(1)
+
+
+Here we use list and dequeue almost interchangeable when performing the merge, however there is one big difference
+between them that must be noted. Index access to an element of the list l[k] is O(1); whereas access to a deque element
+deque[k] is O(n) where n is the size of the array (https://docs.python.org/3/library/collections.html#collections.deque)
+depending on the index we're trying to access.
+
+Therefore instead of the more common approach for the merge using index accesses, we use iterators that have a far
+better performance, both for lists and deques. A test for this can be found in the notebooks folder.
+"""
+from typing import List, Deque, Tuple
+
+
+def k_merge(left: List[int] | Deque[int], right: List[int] | Deque[int]) -> Tuple[List[int], int]:
     """
-    Each of left and right are array of sorted arrays
+    Merges two sorted arrays of int
+
+    This implementation uses iterators and may be confusing if used to the implementation using indexes.
+     - We have the 'current values' for both `left` and `right` arrays, we compare them and add the lowest one to the
+       results list.
+     - We update the current value for the set we inserted into the results, with the next value in the original set.
+       E.G: current_left resulted lower than current_right, therefore we insert current_left into the results list,
+       and then set `current_left = next(iter_left)` both updating the value and advancing in the iteration
+     - We perform these steps until one of the iterators raises an StopIteration. Since the 'current values' were gotten
+       by the `next` function which advances the iterator, before consuming the remaining iterator we mast add one of
+       the `current_values`.
+       E.G: `next(iter_left)` raises StopIteration, therefore before adding all the remaining elements in `iter_right`
+            to the results_list, we must add the `current_right` value to the list.
     """
+    ops = 0
     result = []
-    i = j = 0
+    iter_left = iter(left)
+    iter_right = iter(right)
+    current_left = next(iter_left)
+    current_right = next(iter_right)
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
+    while True:
+        ops += 1
+        if current_left < current_right:
+            try:
+                result.append(current_left)
+                current_left = next(iter_left)
+            except StopIteration:
+                result.append(current_right)
+                ops += 1
+                break
         else:
-            result.append(right[j])
-            j += 1
+            try:
+                result.append(current_right)
+                current_right = next(iter_right)
+            except StopIteration:
+                result.append(current_left)
+                ops += 1
+                break
 
-    result += [left[l] for l in range(i, len(left))]
-    result += [right[r] for r in range(j, len(right))]
+    for l in iter_left:
+        ops += 1
+        result.append(l)
 
-    return result
+    for r in iter_right:
+        ops += 1
+        result.append(r)
+
+    assert ops == len(left) + len(right)
+    return result, ops
 
 
 def merge_sort(arrays, merge_func=k_merge):
-    if len(arrays) == 1:
-        return arrays[0]
+    """
+    Performs the Merge-Sort of a given set of arrays
+    """
+    ops = 0
+    length = len(arrays)
 
-    half = len(arrays) // 2
+    if length == 1:
+        return arrays[0], 1
+
+    half = length // 2
     left = arrays[:half]
     right = arrays[half:]
 
-    sorted_left = merge_sort(left, merge_func)
-    sorted_right = merge_sort(right, merge_func)
+    ops += length
 
-    return merge_func(sorted_left, sorted_right)
+    sorted_left, l_ops = merge_sort(left, merge_func)
+    sorted_right, r_ops = merge_sort(right, merge_func)
+
+    ops += l_ops
+    ops += r_ops
+
+    result, merge_ops = merge_func(sorted_left, sorted_right)
+    ops += merge_ops
+
+    return result, ops
